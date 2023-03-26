056e3fe2682e7fa015e3931fa9e5ad3751bab9ef
Dehuckoo1oo <dehuckoo1oo@mail.ru>
2023-03-27 02:03:57 +0300

    Внедрить интеграционное тестирование и Моки для контроллеров

--- src/test/java/ru/yandex/practicum/ShareIt/services/UserServiceIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/ShareIt/services/UserServiceIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.ShareIt.user.User;
 import ru.yandex.practicum.ShareIt.user.UserDTO;
 import ru.yandex.practicum.ShareIt.user.UserService;
 
